a8765e9fc330c06e634766b58dfce7d60310876b
4lon <alon.nusem@gmail.com>
2025-06-25 17:06:37 +1000

    Added odometry publisher to ros2 node
    This is the message format that this node will output.

--- simple_ros/src/lidar_odometry.cpp
+++ simple_ros/src/lidar_odometry.cpp
@@ -1,4 +1,5 @@
 #include <rclcpp/rclcpp.hpp>
+#include <nav_msgs/msg/odometry.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
 
 using std::placeholders::_1;
@@ -8,6 +9,7 @@ public:
   LidarOdometry() : Node("SiMpLE_lidar_odometry") {
     subPointcloud_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
       "input_cloud", 10, std::bind(&LidarOdometry::pointcloudCallback, this, _1));
+    pubOdom_ = this->create_publisher<nav_msgs::msg::Odometry>("output_odom", 10);
   }
 
 private:
@@ -15,6 +17,7 @@ private:
   }
 
   rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subPointcloud_;
+  rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr pubOdom_;
 };
 
 int main(int argc, char * argv[]) {
