9e3613d191d102de67ac15c0cd483c076d424bc6
4lon <alon.nusem@gmail.com>
2025-06-25 17:06:37 +1000

    Added template for pointcloud topic subscription

--- simple_ros/src/lidar_odometry.cpp
+++ simple_ros/src/lidar_odometry.cpp
@@ -1,13 +1,20 @@
 #include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
 
 using std::placeholders::_1;
 
 class LidarOdometry : public rclcpp::Node {
 public:
   LidarOdometry() : Node("SiMpLE_lidar_odometry") {
+    subPointcloud_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+      "input_cloud", 10, std::bind(&LidarOdometry::pointcloudCallback, this, _1));
   }
 
 private:
+  void pointcloudCallback(const sensor_msgs::msg::PointCloud2 & msg) const {
+  }
+
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subPointcloud_;
 };
 
 int main(int argc, char * argv[]) {
